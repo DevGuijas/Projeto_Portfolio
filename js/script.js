0062be990792f21f845b420ad65967dc817b0ee7
Guilherme Sampaio <104109948+DevGuijas@users.noreply.github.com>
2023-11-16 09:16:37 -0300

    Add files via upload
    ==

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (event) => {
     const imputName = document.getElementById('imputName');
     const textComment = document.getElementById('imputText');
     const form = document.getElementById('formulario');
     const commentPost = document.getElementById('commentPost');
+    
+    // Carregar comentários salvos no armazenamento local ao iniciar a página
+    const savedComments = JSON.parse(localStorage.getItem('comments')) || [];
+    savedComments.forEach(comment => {
+        commentPost.appendChild(createCommentElement(comment));
+    });
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-        let p = document.createElement('p');
-        p.classList = 'p-2 d-flex text-wrap';
-        p.innerHTML = `<strong>${imputName.value}: </strong> &nbsp ${textComment.value}`;
-        commentPost.appendChild(p);
+        const commentData = {
+            name: imputName.value,
+            text: textComment.value
+        };
+
+        // Adicionar comentário à lista de comentários salvos
+        savedComments.push(commentData);
+        localStorage.setItem('comments', JSON.stringify(savedComments));
+
+        // Adicionar comentário ao DOM
+        commentPost.appendChild(createCommentElement(commentData));
+
+        // Limpar campos do formulário
         imputName.value = '';
         textComment.value = '';
-        imputName.focus();
     });
+
+    function createCommentElement(comment) {
+        let p = document.createElement('p');
+        p.classList = 'p-2 d-flex text-wrap';
+        p.innerHTML = `<strong>${comment.name}: </strong> &nbsp ${comment.text}`;
+        return p;
+    }
 });
